6fba0f17738ead028a444f4313dcd53daaf350b7
Anonymous <>
2022-03-12 15:37:55 +1300

    Update

--- deps.ts
+++ deps.ts
@@ -1,1 +1,2 @@
 export * from "https://deno.land/x/harmony@v2.6.0/mod.ts"
+
--- mod.ts
+++ mod.ts
@@ -1,4 +1,3 @@
-// Importing libraries
 import "https://deno.land/x/dotenv@v3.2.0/load.ts";
 
 // Loading environmental variables
@@ -9,25 +8,28 @@ const envTest = Deno.env.get("ENV_TEST");
 console.log(envTest)
 
 // Importing Client and Intents class from Harmony
-import { Client, Intents, Message } from "./deps.ts";
+// import { Client, Intents, Message } from "./deps.ts";
+import { CommandClient, Command, Intents, CommandContext } from "./deps.ts";
 
 // Creating client/bot
-const client = new Client();
+const client = new CommandClient({
+    prefix: ":"
+});
 
 // Listen for when the bot is connected to Discord (i.e. logged in)
 client.on("ready", () => {
     console.log("Bot is ready!");
 })
 
-// Listen for an event which is fired when a Message is sent
-client.on("messageCreate", (message: Message) => {
-    // All the message data is inside 'Message' here.
-    // Content of Message can be accessed using <Message>.content;
-    // here, message.content
-    if (message.content == "!ping") {
-        message.reply("Pong!");
+// Make a class extending Command
+class PingCommand extends Command {
+    name = "ping";
+    execute(ctx: CommandClient) {
+        ctx.message.reply("Pong!");
     }
-})
+}
+
+client.commands.add(PingCommand);
 
 // Proceed with connecting to Discord (login)
 client.connect(token, Intents.None);
